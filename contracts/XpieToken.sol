b6c0cf638cfbcc531d59e54ed99960386b587f12
Zak Pie <zakpie@protonmail.com>
2020-10-23 06:35:37 +0000

    update

--- contracts/XpieToken.sol
+++ contracts/XpieToken.sol
@@ -13,12 +13,25 @@ contract XpieToken is Context, ERC20Pausable, ERC20Burnable {
     string _tokenSymbol = "XPIE";
     uint8 _tokenDecimal = 8;
     uint256 _tokenInitialSupply = uint256(10000000000); //10b
+    address PERMISSION_MANAGER_DOMAIN;
 
-    constructor() public ERC20(_tokenName,_tokenSymbol) {
+    constructor(
+        address _permissionManagerDomain
+    ) public ERC20(_tokenName,_tokenSymbol) {
+
+        PERMISSION_MANAGER_DOMAIN = _permissionManagerDomain;
+
         _setupDecimals(_tokenDecimal);
         uint _tokenTotalSupply = _tokenInitialSupply.mul(10 ** uint256(_tokenDecimal));
         _mint(_owner, _tokenTotalSupply);
     }
+
+    /**
+     * onlyAdmin
+     */
+     modifier onlyAdmin() {
+         
+     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20, ERC20Pausable) {
         super._beforeTokenTransfer(from, to, amount);
